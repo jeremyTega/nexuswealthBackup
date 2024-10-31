68a151542c748b879bd1fb73b73b01675eb1bff5
TegaJeremy <oghenedemartin@gmail.com>
2024-10-31 12:37:54 +0100

    mail

--- middlewares/mail.js
+++ middlewares/mail.js
@@ -1,24 +1,57 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+// const sendEmail = async (options) => {
+//     const transporter = nodemailer.createTransport({
+//         host:"smtp.gmail.com",
+//         service:"gmail",
+//           port:587,
+//         auth: {
+//             user:process.env.user,
+//             pass:process.env.password
+//         },
+//         tls:{
+//             rejectUnauthorized: false,
+//            },
+//     });
+//     const fromAddress = `"${process.env.from_Name}" <${process.env.user}>`;
+
+//     try {
+//         const info = await transporter.sendMail({
+//             from: fromAddress,
+//             to: options.email,
+//             subject: options.subject,
+//             html: options.html
+//         });
+
+//         console.log("Message sent: %s", info.messageId);
+//         return info;
+//     } catch (error) {
+//         console.error("Error sending email:", error);
+//         throw error;
+//     }
+// };
+
+
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
-        service:"gmail",
-          port:587,
+        host: "smtp.zoho.com",  // Use Zoho SMTP server
+        port: 465,  // Use 465 for SSL
+        secure: true,  // true for 465, false for other ports like 587
         auth: {
-            user:process.env.user,
-            pass:process.env.password
+            user: process.env.ZOHO_USER,  // Your Zoho email address
+            pass: process.env.ZOHO_PASSWORD  // Your Zoho password or app-specific password
         },
-        tls:{
+        tls: {
             rejectUnauthorized: false,
-           },
+        },
     });
-    const fromAddress = `"${process.env.from_Name}" <${process.env.user}>`;
+
+    const fromAddress = `"${process.env.FROM_NAME}" <${process.env.ZOHO_USER}>`;
 
     try {
         const info = await transporter.sendMail({
-            from: fromAddress,
+            from:fromAddress,
             to: options.email,
             subject: options.subject,
             html: options.html
@@ -31,6 +64,14 @@ const sendEmail = async (options) => {
         throw error;
     }
 };
+
+
+
+
+
+
+
+module.exports = sendEmail;
 
 
 
